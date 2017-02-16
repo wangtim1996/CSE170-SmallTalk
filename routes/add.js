b253e6fa5b77e1790d744fef1fdf77854aa25a00
Timothy Wang <wangtim1996@gmail.com>
2017-02-15 18:04:21 -0800

    log,history,entry functionality done

---
+++ routes/add.js
@@ -0,0 +1,17 @@
+var data = require("../data.json");
+
+exports.addEntry = function(req, res) {
+	// Your code goes here
+  var date = new Date().getTime();
+  var title = req.query.title;
+  var log = req.query.log;
+
+  var entry = {
+    "date" : date,
+    "title" : title,
+    "desc" : log
+  };
+  console.log(entry);
+  data.entries.push(entry);
+  res.redirect('/');
+}
--- routes/history.js
+++ routes/history.js
@@ -1,26 +1,7 @@
+var data = require('../data');
+
 exports.view = function(req, res){
   //some kind of call to the database or something, oh wait, it's asynchronous
   //idk man.
-  res.render('history', {
-    'logs' : [
-      { 'date': '20170125'
-      },
-      { 'date': '20170127'
-      },
-      { 'date': '20170201'
-      },
-      { 'date': '20170203'
-      },
-      { 'date': '20170204'
-      },
-      { 'date': '20170205'
-      },
-      { 'date': '20170208'
-      },
-      { 'date': '20170210'
-      }
-
-
-    ]
-  });
+  res.render('history', data);
 };
