1897c24304eaed405726a86eeffdce7a0b6acc51
Timothy Wang <wangtim1996@gmail.com>
2017-02-26 18:14:37 -0800

    entry textarea, log line wrap, random challenge only in one place

--- public/js/randomChallenge.js
+++ public/js/randomChallenge.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	setRandom();
 })
 
 /*
@@ -16,16 +17,21 @@ function randomizeChallenge(e)
 {
   e.preventDefault();//prob not even needed, lol
 
-  var challenges = Array(
-    'Say hi to a random stranger.',
-    'Ask a stranger a question.',
-    'Complement a boy',
-    'Act insane in front of people',
-    'IDK, Figure it out yourself',
-    'Complement a girl'
-  );
-  var challenge = challenges[Math.floor(Math.random()*challenges.length)];
+	setRandom();
 
-  $('.theChallenge').html(challenge);
+}
 
+function setRandom()
+{
+	var challenges = Array(
+		'Say hi to a random stranger.',
+		'Ask a stranger a question.',
+		'Complement a boy',
+		'Act insane in front of people',
+		'IDK, Figure it out yourself',
+		'Complement a girl'
+	);
+	var challenge = challenges[Math.floor(Math.random()*challenges.length)];
+
+	$('.theChallenge').html(challenge);
 }
--- routes/challenge.js
+++ routes/challenge.js
@@ -1,15 +1,3 @@
 exports.view = function(req, res){
-  var challenges = Array(
-    'Say hi to a random stranger.',
-    'Ask a stranger a question.',
-    'Complement a boy',
-    'Act insane in front of people',
-    'IDK, Figure it out yourself',
-    'Complement a girl'
-  );
-  var challenge = challenges[Math.floor(Math.random()*challenges.length)];
-
-  res.render('challenge',{
-    'challenge' : challenge
-  });
+  res.render('challenge');
 };
